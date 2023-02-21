3f4496963164eb9fbe0f2ae510740dfa45a89be2
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-02-21 18:38:52 +0200

    task 1

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
-import {} from './Feadback.styled';
+import { ListFeedback, ItemFeedback, Button } from './Feadback.styled';
 
 export const FeedbackOptions = ({ incrementNeutral, incrementGood, incrementBad }) => (
-        <ul>
-        <li><button type="button" onClick={() => incrementGood()}>Good</button></li>
-        <li><button type="button" onClick={() => incrementNeutral()}>Neutral</button></li>
-        <li><button type="button" onClick={() => incrementBad()}>Bad</button></li>
-        </ul>
+        <ListFeedback>
+        <ItemFeedback><Button type="button" onClick={() => incrementGood()}>Good</Button></ItemFeedback>
+        <ItemFeedback><Button type="button" onClick={() => incrementNeutral()}>Neutral</Button></ItemFeedback>
+        <ItemFeedback><Button type="button" onClick={() => incrementBad()}>Bad</Button></ItemFeedback>
+        </ListFeedback>
 )
 
 FeedbackOptions.propTypes = {
--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -1,15 +1,16 @@
 import PropTypes from "prop-types";
+import { ListStatistics, ItemStatistics } from './Feadback.styled'
 
 export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
     <>
     <h2>Statistics</h2>
-        <ul>
-          <li>Good: {good}</li>
-          <li>Neutral: {neutral}</li>
-          <li>Bad: {bad}</li>
-          <li>Total: {total()}</li>
-          <li>Positive feedback: {positivePercentage()}%</li>
-        </ul>
+        <ListStatistics>
+          <ItemStatistics>Good: {good}</ItemStatistics>
+          <ItemStatistics>Neutral: {neutral}</ItemStatistics>
+          <ItemStatistics>Bad: {bad}</ItemStatistics>
+          <ItemStatistics>Total: {total()}</ItemStatistics>
+          <ItemStatistics>Positive feedback: {positivePercentage()}%</ItemStatistics>
+        </ListStatistics>
     </>
 )
 
